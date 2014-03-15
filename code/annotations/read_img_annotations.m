ce85d1017c0b38344e39270ea9006cf814b4ad4d
Georgia Gkioxari <gkioxari@berkeley.edu>
2014-03-14 17:57:55 -0700

    all flip

--- code/annotations/read_img_annotations.m
+++ code/annotations/read_img_annotations.m
@@ -1,4 +1,4 @@
-function annot=read_img_annotations(img_names,annot_path)
+function annot=read_img_annotations(img_names,imglist,annot_path)
 %% READ_IMG_ANNOTATIONS() returns the annotations of the images
 %% INPUT
 % img_names     : Nx1 cell of image names
@@ -8,6 +8,7 @@ function annot=read_img_annotations(img_names,annot_path)
 %                 coords : Kpx3xD single of the [x y z] of the Kp keypoints
 %                 visible: KpxD binary of the visibility of the keypoints
 %                 bounds : Dx4 single of the visible bounding boxes [x y w h]
+%            img_flipped : Dx1 logical of whether annotation is flipped
 %               img_name : Dx1 cell of the image names
 %                 voc_id : Dx1 uint8 of the voc_id 
 %               entry_id : Dx1 uint16 of the index
@@ -18,6 +19,10 @@ function annot=read_img_annotations(img_names,annot_path)
 kps = {'L_Shoulder','L_Elbow','L_Wrist','R_Shoulder','R_Elbow','R_Wrist',...
     'L_Hip','L_Knee','L_Ankle','R_Hip','R_Knee','R_Ankle',...
     'L_Eye','R_Eye','L_Ear','R_Ear','Nose','B_Head'};
+
+flip_kps = [4 5 6 1 2 3 10 11 12 7 8 9 14 13 16 15 17 18];
+
+
 num_kps = length(kps);
 
 % Initialize annot struct
@@ -25,6 +30,7 @@ num=0;
 annot.coords=single(nan(numel(kps),3,num));
 annot.visible=false(numel(kps),num);
 annot.bounds=single(zeros(num,4));
+annot.img_flipped = false(zeros,1);
 annot.img_name = cell(num,1);
 annot.voc_id = uint8(zeros(num,1));
 annot.entry_id = uint16(zeros(num,1));
@@ -87,9 +93,38 @@ for i=1:length(img_names)
         annot.coords(:,:,num) = temp_coords;
         annot.visible(:,num) = temp_visible;
         annot.entry_id(num,1)=num;
+        annot.img_flipped(num,1)=false;
         fclose(fid);
         
     end
     
 end
+
+% flip annotations
+annot2 = flip_annotations(annot,flip_kps,imglist);
+annot = struct_concat(annot,annot2);
+
+% -------------------------------------------------------------------------
+function annot = flip_annotations(annot,flip_kps,imglist)
+
+annot_flipped = annot;
+num = size(annot.coords,3);
+for i=1:num 
+    img_dims = imglist(strcmp(annot.img_name{i},{imglist.id})).dims;
+    w = img_dims(1);
+    annot_flipped.coords(:,1,i)=w-annot_flipped.coords(:,1,i)+1;
+    annot_flipped.coords(:,:,i) = annot_flipped.coords(flip_kps,:,i);
+    annot_flipped.visible(:,i) = annot_flipped.visible(flip_kps,i);
+    annot_flipped.img_flipped(i,1)=true;
+    x1 = annot.bounds(i,1); y1 = annot.bounds(i,2);
+    x2 = annot.bounds(i,1)+annot.bounds(i,3); y2 = annot.bounds(i,2)+annot.bounds(i,4);
+    x1 = w-x1+1; x2 = w-x2+1;
+    bounds = [min([x1 y1;x2 y2],[],1) max([x1 y1;x2 y2],[],1)-min([x1 y1;x2 y2],[],1)];
+    annot_flipped.bounds(i,:)=bounds; clear bounds;
+    annot_flipped.entry_id(i,:)=i+num;
+end
+
+
+
+% -------------------------------------------------------------------------
 
--- code/annotations/struct_concat.m
+++ code/annotations/struct_concat.m
@@ -20,20 +20,3 @@ for i=1:numel(fields)
 end
 a.entry_id=uint16([1:size(a.coords,3)])';
 
-
-% % make sure you keep unique entries
-% keep = true(size(a.coords,3),1);
-% names = unique(a.img_name);
-% for i=1:numel(names)
-%     ind = find(strcmp(names{i},a.img_name));
-%     ids = a.id_in_voc(ind);
-%     ids = unique(ids);
-%     for j=ids'
-%         iind = find(strcmp(names{i},a.img_name) & a.id_in_voc==j);
-%         if length(iind)>=2
-%             keep(iind(2:end))=false;
-%         end
-%     end
-% 
-% end
-% a = struct_select(a,keep);
