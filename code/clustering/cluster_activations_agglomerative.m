3d40c2432d6b3be9a27c6011cda9a7d5d5c3572a
Georgia Gkioxari <gkioxari@berkeley.edu>
2014-03-28 16:36:19 -0700

    cluster

--- code/clustering/cluster_activations_agglomerative.m
+++ code/clustering/cluster_activations_agglomerative.m
@@ -1,57 +1,53 @@
-function [clusters]=cluster_activations_agglomerative(boxes,kps,dtc_prec,thresh)
+function clusters = cluster_activations_agglomerative...
+            (detections, kps_models, pr, target_kps, thresh)
+%% CLUSTER_ACTIVATIONS_AGGLOMERATIVE() returns clusters of activations 
+%% by clustering them based on target kps overlap
+%% INPUT
+% detections    : detections of poselets
+% kps_models    : keypoint models
+% pr            : prec-rec mappings for poselets
+% target_kps    : set of target keypoints
+% thresh        : overlap threshold for clustering
+%% OUTPUT
+% clusters      : Cx1 structure of clusters, where
+%                members is a Nx1 vector of indices in detections
+%                imid is a scalar, unique for a given image 
 
-Kp = size(kps.kps_mean,1);
-torso_ks = [1 4 7 10];
 ci=0;
-
-image_ids = unique(boxes(:,end))';
+imids = unique(detections.imids)';
 
 for imid=image_ids
     
-    keep = find(boxes(:,end)==imid);
+    keep = find(detections.imid==imid);
     N = length(keep);
     if N==0, continue; end
-    fprintf('Image Id %d\n',imid);
+
+    fprintf('Imid %d\n',imid);
     fprintf('...Getting torso bounds\n');
     torso_bounds = nan(N,4);
     scores = nan(N,1);
     for i=1:N
-        kid = boxes(keep(i),end-1);
-        K = kps.num_parts(kid);
-        bb = reshape(boxes(keep(i),1:4*K),[4 K])';
-        ctr = (bb(:,1:2)+bb(:,3:4))/2;
-        width = bb(:,3)-bb(:,1);
-        tt = zeros(Kp,2);
-        for k=1:K
-            temp_coords =kps.kps_mean(:,:,k,kid)*width(k);
-            temp_coords = bsxfun(@plus,temp_coords,ctr(k,:));
-            tt=tt+bsxfun(@times,temp_coords,kps.kps_weights(:,k,kid));
-        end
-        tt = tt(torso_ks,:);
-        torso_bounds(i,:) = [min(tt,[],1) max(tt,[],1)-min(tt,[],1)];
+        kid = detections.kpids(keep(i));
+        box = detections.boxes(keep(i),:);
+        scr = detections.scores(keep(i));
+        coords = predict_keypoints(box,kps_models(kid));
+        cc = coords(target_kps,:);
+        torso_bounds(i,:) = [min(cc,[],1) max(cc,[],1)-min(cc,[],1)];
+        clear cc coords box;
         
-        [m mi]=min(abs(dtc_prec(kid).scores-boxes(keep(i),end-2)));
-        scores(i)=dtc_prec(kid).prec(mi);
+        [m mi] = min(abs(pr(kid).scores-scr));
+        scores(i) = pr(kid).prec(mi);
     end
+    
     fprintf('...Computing IOU\n');
     iou = inters_union(torso_bounds,torso_bounds);
-    fprintf('...Computing dist\n');
-%    dist = inf(1,N*(N-1)/2);
-%    di=0;
- %   for j=1:N
-  %      for i=j+1:N
-  %          di = di+1;
-   %         dist(di)=1-iou(i,j);
-   %     end
-   % end
     
     fprintf('...Agglomerative Clustering\n');
-%     Z = linkage(dist,'complete');
-    Z=leaderlinkage2(1-iou, scores);
+    Z=leaderlinkage(1-iou, scores);
     if ~isempty(Z)
-    	T = cluster(Z,'cutoff',thresh,'criterion','distance');
+    	T = cluster(Z,'cutoff',1-thresh,'criterion','distance');
     else
-	T=1;
+        T = 1;
     end
     num_clust = max(T);
     for i=1:num_clust
